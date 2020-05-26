5049c4713ea7a1f7182c3e9f8ab907621bb5f5a4
Nayalash <1nayalashm@gmail.com>
2020-05-26 14:31:47 -0400

    Big O Uploaded

---
+++ BigO/Exercise1.js
@@ -0,0 +1,18 @@
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+
+function funChallenge(input) {
+    let a = 10;
+    a = 50 + 3;
+  
+    for (let i = 0; i < input.length; i++) {
+      anotherFunction();
+      let stranger = true;
+      a++;
+    }
+    return a;
+  }
+
+  funChallenge();
+
+  //BIG 0 -> O(4n+3)
+
